3d143880871939dd47a32d96be6cf7592236e6d8
Leigh Michael Forrest <leighmforrest@gmail.com>
2016-12-28 18:44:37 -0500

    markers ready

--- js/map.js
+++ js/map.js
@@ -1,5 +1,3 @@
-// Create an object literal of locations
-//var markers = []; // Array of markers.
 /*
     Array of attractions. Note: Attraction object holds a reference to a markers
     object.
@@ -7,18 +5,6 @@
 var attractionList = [];
 var map;
 
-// Define a constructor for an Attraction.
-function Attraction(data){
-    this.name = data.name;
-    this.lat = data.lat;
-    this.lng = data.lng;
-    this.marker;
-};
-
-// Method adds a reference to a marker
-Attraction.prototype.addMarker = function (marker){
-    this.marker = marker;
-}
 
 function initMap() {
   var center = {lat: 38.98586, lng:-74.82464};
@@ -31,6 +17,37 @@ function initMap() {
   attractions.forEach(function(attraction){
       attractionList.push(addAttraction(attraction));
   });
+
+}
+
+// Define a constructor for an Attraction.
+function Attraction(data){
+    this.name = ko.observable(data.name);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+    this.marker;
+    this.infowindow = new google.maps.InfoWindow({
+    content: this.name()
+  });
+};
+
+// Method adds a reference to a marker
+Attraction.prototype.addMarker = function (marker){
+    this.marker = marker;
+}
+
+Attraction.prototype.addClickEvent = function (f){
+    // Note: make sure the passed in function can access this!
+    google.maps.event.addListener(this.marker, 'click', f.bind(this));
+};
+
+/* Function creates an Attraction, creates a marker, and returns Attraction. */
+function addAttraction(data){
+    var attraction = new Attraction(data);
+    var marker = addMarker(data);
+    attraction.addMarker(marker);
+    attraction.addClickEvent(function() {this.infowindow.open(map, marker);});
+    return attraction;
 }
 
 /* Creates a marker and returns it in order to add reference to it in an
@@ -39,16 +56,9 @@ function addMarker(location) {
     // create a marker
     var marker = new google.maps.Marker({
       position: {lat: location.lat, lng: location.lng},
-      map: map
+      map: map,
+      animation:google.maps.Animation.DROP
     });
 
     return marker;
-}
-
-/* Function creates an Attraction, creates a marker, and returns Attraction. */
-function addAttraction(data){
-    var attraction = new Attraction(data);
-    var marker = addMarker(data);
-    attraction.addMarker(marker);
-    return attraction;
 }
