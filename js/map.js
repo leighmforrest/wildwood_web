52e390c4f6acd43c846127294780b2262de09a35
Leigh Michael Forrest <leighmforrest@gmail.com>
2016-12-29 17:59:14 -0500

    more code

--- js/map.js
+++ js/map.js
@@ -8,38 +8,45 @@ var atractionInfoWindow;
 
 
 function initMap() {
-    var center = {lat: 38.98586, lng:-74.82464};
+    var center = {
+        lat: 38.98586,
+        lng: -74.82464
+    };
     map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: center
+        zoom: 13,
+        center: center
     });
 
     myinfowindow = new google.maps.InfoWindow();
 
+    // For each attraction, make the marker and add the event listener.
     for (i = 0; i < attractions.length; i++) {
-    //console.log(locations[i]); // check
-    var attraction = attractions[i];
-    marker = new google.maps.Marker({
-        map: map,
-        title: attraction.title,
-        position: {lat: attraction.lat, lng: attraction.lng},
-    });
-
-    // add marker to markers
-    markers.push(marker);
-    (function(marker, attraction) {
-
-        // When a marker is clicked
-        google.maps.event.addListener(marker, "click", function() {
-            myinfowindow.open(map, marker);
-            console.log(attraction); //check
-            myinfowindow.setContent(attraction.name);
-            // marker.setIcon({
-            //     url: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-            // });
+        var attraction = attractions[i];
+        marker = new google.maps.Marker({
+            map: map,
+            title: attraction.title,
+            position: {
+                lat: attraction.lat,
+                lng: attraction.lng
+            },
         });
-    })(marker, attraction);
-}
+
+        // add marker to markers
+        markers.push(marker);
+        (function(marker, attraction) {
+
+            // When a marker is clicked
+            google.maps.event.addListener(marker, "click", function() {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function(){ marker.setAnimation(null); }, 750);
+                myinfowindow.open(map, marker);
+                myinfowindow.setContent(attraction.name);
+                // marker.setIcon({
+                //     url: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+                // });
+            });
+        })(marker, attraction);
+    }
 
 }
 
@@ -55,32 +62,36 @@ ViewModel = function() {
         //console.log(locations[i].title.toLowerCase());
     }
 
-    console.log(self.attractionList());
-    console.log(self.attractionList().title);
-    self.filteredAttractions = ko.computed(function(){
+    self.filteredAttractions = ko.computed(function() {
         // filter must be function by itself; do not use toLowerCase() here!
         var filter = self.filter();
-        if(!filter){
-          // make all markers visible
-          for (var i = 0; i < markers.length; i++) {
-            markers[i].setVisible(true);
-        }
-          return self.attractionList();
-        }else{
-          // filter array if no text in search box
-          return ko.utils.arrayFilter(self.attractionList(),
-          function(attraction, i){
-              var found = attraction.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
-              markers[i].setVisible(found);
-              return found;
-          });
+        if (!filter) {
+            // make all markers visible
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setVisible(true);
+            }
+            return self.attractionList();
+        } else {
+            // filter array if no text in search box
+            return ko.utils.arrayFilter(self.attractionList(),
+                function(attraction, i) {
+                    var found = attraction.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+                    markers[i].setVisible(found);
+                    return found;
+                });
         }
     });
+
+    this.openInfo = function(thisList) {
+      for (var i = 0; i < markers.length; i++) {
+          console.log(markers[i].title);
+      }
+    };
 };
 
 ko.applyBindings(new ViewModel());
 
 /*Handle an error when attempting to load the map API. */
-function error(){
+function error() {
     alert("The Google Maps API could not be loaded.");
 }
